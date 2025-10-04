3def86f993f35b804486308fa89169800e94b72f
wcbing <cbing2002@outlook.com>
2025-10-04 12:49:01 +0800

    mv check to CI; add remote trigger

--- build.sh
+++ build.sh
@@ -3,18 +3,12 @@
 PACKAGE="dufs"
 REPO="sigoden/dufs"
 
-VERSION="$(cat tag)"
+# Processing again to avoid errors of remote incoming 
+VERSION=$(echo $1 | sed -n 's|[^0-9]*\([^_]*\).*|\1|p')
 
 ARCH="amd64 arm64"
 AMD64_FILENAME="dufs-v$VERSION-x86_64-unknown-linux-musl.tar.gz"
 ARM64_FILENAME="dufs-v$VERSION-aarch64-unknown-linux-musl.tar.gz"
-
-get_url_by_arch() {
-    case $1 in
-    "amd64") echo "https://github.com/$REPO/releases/latest/download/$AMD64_FILENAME" ;;
-    "arm64") echo "https://github.com/$REPO/releases/latest/download/$ARM64_FILENAME" ;;
-    esac
-}
 
 build() {
     # Prepare
@@ -29,7 +23,15 @@ build() {
     mv "$PACKAGE" "$BASE_DIR/usr/bin/$PACKAGE"
     chmod 755 "$BASE_DIR/usr/bin/$PACKAGE"
     # Build
-    dpkg-deb --build --root-owner-group -Z xz "$BASE_DIR"
+    dpkg-deb -b --root-owner-group -Z xz "$BASE_DIR" output
+}
+
+get_url_by_arch() {
+    DOWNLOAD_PERFIX="https://github.com/$REPO/releases/latest/download"
+    case $1 in
+    "amd64") echo "$DOWNLOAD_PERFIX/$AMD64_FILENAME" ;;
+    "arm64") echo "$DOWNLOAD_PERFIX/$ARM64_FILENAME" ;;
+    esac
 }
 
 for i in $ARCH; do
@@ -38,5 +40,6 @@ for i in $ARCH; do
 done
 
 # Create repo files
+cd output
 apt-ftparchive packages . > Packages
 apt-ftparchive release . > Release
